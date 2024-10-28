2987f9d08386c317016b1f0a66b2a2fa2bcbf652
SergioBlascoCano <101952251+SergioBlascoCano@users.noreply.github.com>
2024-10-28 21:33:22 +0100

    C++ 0242

--- C++/0242-valid-anagram.cpp
+++ C++/0242-valid-anagram.cpp
@@ -1,12 +1,29 @@
 /////////////////////////////////////////////////////////////////////////////////////
-// https://leetcode.com/problems/contains-duplicate/description/
 // 0242 - Valid Anagram
 // Difficulty: EASY
+// Exercise: https://leetcode.com/problems/contains-duplicate/description/
+// Solution: https://leetcode.com/problems/valid-anagram/submissions/1436611033/
 /////////////////////////////////////////////////////////////////////////////////////
+
+#include <string>
+#include <vector>
 
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    bool isAnagram(std::string s, std::string t) {
+        if (s.size() != t.size()) return false;
         
+        std::vector<int> count(26, 0);
+        
+        for (int i = 0; i < s.size(); ++i) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        
+        for (int i = 0; i < 26; ++i) {
+            if (count[i] != 0) return false;
+        }
+        
+        return true;
     }
 };
