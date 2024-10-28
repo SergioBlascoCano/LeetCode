7ddd92bd2a6db672b7348f8c7b07214c90b3b6bd
Sergio Blasco <sergioblascoc3@gmail.com>
2024-10-28 17:48:49 +0100

    C++: 0001

---
+++ C++/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// https://leetcode.com/problems/valid-anagram/description/
+// 0242 - Valid Anagram
+// Difficulty: EASY
+/////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        
+    }
+}
