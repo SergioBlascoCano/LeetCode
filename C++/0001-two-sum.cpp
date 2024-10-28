b9cd667ea11c4e136006eb10a542f02685b18ef9
SergioBlascoCano <101952251+SergioBlascoCano@users.noreply.github.com>
2024-10-28 21:08:06 +0100

    C++: 0001

--- C++/0001-two-sum.cpp
+++ C++/0001-two-sum.cpp
@@ -4,9 +4,20 @@
 // Difficulty: EASY
 /////////////////////////////////////////////////////////////////////////////////////
 
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> num_map;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (num_map.find(complement) != num_map.end()) {
+                return std::vector<int>{num_map[complement], i};
+            }
+            num_map[nums[i]] = i;
+        }
+        return std::vector<int>();
     }
 };
