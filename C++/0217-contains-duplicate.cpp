b2197e02a0d630799a01fe47ae09047085e51642
SergioBlascoCano <101952251+SergioBlascoCano@users.noreply.github.com>
2024-10-29 15:12:38 +0100

    C++ 0217

--- C++/0217-contains-duplicate.cpp
+++ C++/0217-contains-duplicate.cpp
@@ -2,12 +2,21 @@
 // 0217 - Contains Duplicate
 // Difficulty: EASY
 // Exercise: https://leetcode.com/problems/contains-duplicate/description/
-// Solution: 
+// Solution: https://leetcode.com/problems/contains-duplicate/submissions/1437285910
 /////////////////////////////////////////////////////////////////////////////////////
+
+#include <vector>
+#include <algorithm>
 
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
     }
 };
