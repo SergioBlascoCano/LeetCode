a6e7dead8eadbc2a750282711ecd8a88b2a1a0f0
SergioBlascoCano <101952251+SergioBlascoCano@users.noreply.github.com>
2024-11-07 12:55:47 +0100

    C++ 2275

---
+++ C++/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// 2275 - Add Two Numbers
+// Difficulty: MEDIUM
+// Exercise: https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/description/
+// Solution: https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/submissions/1445719678
+/////////////////////////////////////////////////////////////////////////////////////
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int largestCombination(std::vector<int>& candidates) {
+        int bit_counts[32] = {0};
+        for (int num : candidates) {
+            for (int bit = 0; bit < 32; ++bit) {
+                if (num & (1 << bit)) {
+                    ++bit_counts[bit];
+                }
+            }
+        }
+        return *std::max_element(bit_counts, bit_counts + 32);
+    }
+};
